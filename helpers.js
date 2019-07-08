dd57e3de888d1690ed026b529552fe0748d7f396
Mateusz Siniarski <matthews@headuplabs.com>
2019-07-09 09:20:38 +1000

    Copy solution (n fields)

---
+++ helpers.js
@@ -0,0 +1,38 @@
+module.exports = {
+  getSum: obj => Object.values(obj).reduce((a, value) => a + value, 0),
+  outputResult: obj =>
+    Object.entries(obj).reduce(
+      (a, [key, value], index) =>
+        `${a}${index !== 0 ? ", " : ""}${key} = ${value}`,
+      ""
+    ),
+  getInitialObject: fields => fields.reduce((a, c) => ({ ...a, [c]: 0 }), {}),
+  getNextObject: (obj, min = 0, max = 10) =>
+    Object.entries(obj).reduce(
+      (a, [key, value], index) => {
+        if (index === 0 || a.carry === true) {
+          let newValue = value + 1;
+          let carry = false;
+          if (newValue > max) {
+            newValue = min;
+            carry = true;
+          }
+          return {
+            newObj: {
+              ...a.newObj,
+              [key]: newValue
+            },
+            carry
+          };
+        }
+        return {
+          newObj: {
+            ...a.newObj,
+            [key]: value
+          },
+          carry: false
+        };
+      },
+      { newObj: {}, carry: false }
+    )
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+const {
+  getInitialObject,
+  getNextObject,
+  getSum,
+  outputResult
+} = require("./helpers");
+
+const outputSums = (fields, min, max, total) => {
+  let obj = getInitialObject(fields);
+  while (true) {
+    if (getSum(obj) === total) {
+      console.log(outputResult(obj));
+    }
+
+    const { newObj, carry } = getNextObject(obj, min, max);
+    obj = { ...newObj };
+
+    if (carry) {
+      break;
+    }
+  }
+};
+
+outputSums(["a", "b", "c", "d", "e"], 0, 10, 10);
